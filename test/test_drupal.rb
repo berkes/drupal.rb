6afcc106aa7bf59f6e4174ee1f32ca4cd052d646
Bèr Kessels <ber@webschuur.com>
2010-02-14 14:00:54 +0100

    clean out. Lets start with a jewlered version.

--- test/test_drupal.rb
+++
@@ -1,6 +0,0 @@
-
-require 'test/unit'
-require File.dirname(__FILE__) + '/../lib/drupal.rb'
-require File.dirname(__FILE__) + '/test_install.rb'
-require File.dirname(__FILE__) + '/test_create_module.rb'
-require File.dirname(__FILE__) + '/test_todo_list.rb'
