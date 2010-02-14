1a924827ea10910c56790ea3c711cf78ed1cdcb6
Bèr Kessels <ber@webschuur.com>
2010-02-14 15:26:05 +0100

    update of cache: tests written.

--- test/test_drupal.rb
+++ test/test_drupal.rb
@@ -1,7 +1,30 @@
 require 'helper'
 
 class TestDrupalRb < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  context "Update" do
+    setup do
+      start_time = Time.new()
+      Update.all
+      cache_path = File.join(File.dirname(__FILE__), '..', 'cache')
+      cache_file = File.join(cache_path, 'projects.xml')
+    end
+    
+    should "cache directory must be writable" do
+      assert File.new('test', 'w+')
+      assert File.unlink('test')
+    end
+
+    should "file must exist" do
+      assert File.exists? cache_file
+    end
+    
+    should "file must be accessed just now" do
+      assert File.new(cache_file).ctime >= start_time
+    end
+    
+    should "file must not be empty" do
+      assert File.size?(cache_file)
+    end
+    
   end
 end
